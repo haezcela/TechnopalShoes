2acf9a5a631713e114b53ca755ae10d66992133e
haezcela <aleczhysa@gmail.com>
2025-03-24 02:09:20 -0400

    Add Item management classes and methods for CRUD operations and utilities
    - Create `ItemAction` class to handle session variables
    - Create `ItemActionAjax` class for AJAX-based item actions
    - Implement `setInput` method to set item attributes from request
    - Implement `validateInput` method to validate item attributes
    - Implement `executeLogic` method to handle CRUD operations
    - Implement `dataTableAction` and `search` methods for data table
    actions
    - Create `ItemDAO` class for database operations
    - Implement methods for add, update, delete, and list items
    - Create `ItemDTO` class to represent item data
    - Create `ItemUtil` class for utility functions

---
+++ TechnopalShoes/src/main/java/item/ItemDTO.java
@@ -0,0 +1,96 @@
+package com.laponhcet.item;
+import com.laponhcet.itemcategory.ItemCategoryDTO;
+import com.laponhcet.itemunit.ItemUnitDTO;
+import com.mytechnopal.base.DTOBase;
+import com.mytechnopal.util.StringUtil;
+
+public class ItemDTO extends DTOBase {
+	private static final long serialVersionUID = 1L;
+	public static final String SESSION_ITEM = "SESSION_ITEM";
+	public static final String SESSION_ITEM_LIST = "SESSION_ITEM_LIST";
+	public static final String SESSION_ITEM_DATA_TABLE = "SESSION_ITEM_DATA_TABLE";
+	public static final String ACTION_SEARCH_BY_NAME = "ACTION_SEARCH_BY_NAME";
+	
+	private ItemCategoryDTO itemCategory;
+	private String name;
+	private String description;
+	private ItemUnitDTO itemUnit;
+	private Double quantity;
+	private Double reorderPoint;
+
+	public ItemDTO() {
+		super();
+		itemCategory = new ItemCategoryDTO();
+		itemUnit = new ItemUnitDTO();
+		name = "";
+		description = "";
+		quantity= (double) 1;
+		reorderPoint=(double) 3;
+	}
+	
+	public ItemDTO getItem() { 
+		ItemDTO item = new ItemDTO();
+		item.setItemCategory(this.itemCategory);
+		item.setItemUnit(this.itemUnit);
+		item.setName(this.name);
+		item.setDescription(this.description);
+		item.setQuantity(this.quantity);
+		item.setReorderpoint(this.reorderPoint);
+		return item;
+	}
+
+	public ItemCategoryDTO getItemCategory() {
+		return itemCategory;
+	 }
+	
+	public void setItemCategory(ItemCategoryDTO itemCategory) {
+		this.itemCategory = itemCategory;
+	 }
+	
+	public ItemUnitDTO getItemUnit() {
+		return itemUnit;
+	 }
+	public void setItemUnit(ItemUnitDTO itemUnit) {
+		this.itemUnit = itemUnit;
+	 }
+	
+	public String getName() {
+		return name;
+	 }
+	
+	public void setName(String name) {
+		this.name = name;
+	 }
+	
+	public String getDescription() {
+		return description;
+	 }
+	
+	public void setDescription(String description) {
+		this.description = description;
+	 }
+	
+	public Double getQuantity() {
+		return quantity;
+	 }
+	
+	public void setQuantity(Double quantity) {
+		this.quantity = quantity;
+	 }
+	
+	public Double getReorderpoint() {
+		return reorderPoint;
+	 }
+	
+	public void setReorderpoint(Double reorder_point) {
+		this.reorderPoint = reorder_point;
+	 }
+	
+	
+	//add list of itemmedia DTO
+	
+	
+	
+	
+
+}
